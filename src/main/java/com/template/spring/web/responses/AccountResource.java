6699dfa9d8a64ef544c87f1df725edbf7b54a607
N3oxX <d>
2024-10-07 11:13:07 +0200

    lombok http collections

--- src/main/java/com/template/spring/web/responses/AccountResource.java
+++ src/main/java/com/template/spring/web/responses/AccountResource.java
@@ -1,46 +1,13 @@
 package com.template.spring.web.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AccountResource {
 
     private final long accountNumber;
     private final long balanceInCents;
 
-    public AccountResource(long accountNumber, long balanceInCents) {
-        this.accountNumber = accountNumber;
-        this.balanceInCents = balanceInCents;
-    }
-
-    public long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public long getBalanceInCents() {
-        return balanceInCents;
-    }
-
-    @Override
-    public String toString() {
-        return "AccountResource{" +
-                "accountNumber=" + accountNumber +
-                ", balanceInCents=" + balanceInCents +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AccountResource that = (AccountResource) o;
-
-        if (accountNumber != that.accountNumber) return false;
-        return balanceInCents == that.balanceInCents;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (accountNumber ^ (accountNumber >>> 32));
-        result = 31 * result + (int) (balanceInCents ^ (balanceInCents >>> 32));
-        return result;
-    }
 }
