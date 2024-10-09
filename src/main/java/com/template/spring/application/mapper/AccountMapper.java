ed08eecb43a427bffa514f3052da61db5632999c
N3oxX <d>
2024-10-09 15:45:15 +0200

    generic mapper

--- src/main/java/com/template/spring/application/mapper/AccountMapper.java
+++ src/main/java/com/template/spring/application/mapper/AccountMapper.java
@@ -6,13 +6,11 @@ import com.template.spring.web.dto.output.AccountDTOResponse;
 import com.template.spring.domain.model.Account;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+
 import org.mapstruct.factory.Mappers;
 
-import java.util.List;
-
 @Mapper
-public interface AccountMapper extends ParamMapper<Account, AccountDTO, AccountDBO>{
+public interface AccountMapper extends EntityMapper<Account, AccountDTO, AccountDBO> {
 
   AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
 
@@ -23,14 +21,3 @@ public interface AccountMapper extends ParamMapper<Account, AccountDTO, AccountD
 
 }
 
-interface ParamMapper<E, D ,B> {
-
-  E toEntity(D dto);
-  B EntityToDBO(E entity);
-  E DBOToEntity(B dbo);
-  D toDto(E entity);
-
-  List<D> listToDtoList(List<E> list);
-  E updateEntity(@MappingTarget E entity, D dto);
-
-}
---
+++ src/main/java/com/template/spring/application/mapper/EntityMapper.java
@@ -0,0 +1,14 @@
+package com.template.spring.application.mapper;
+
+interface EntityMapper<E, T, B> {
+
+    E toEntity(T dto);
+
+    B EntityToDBO(E entity);
+
+    E DBOToEntity(B dbo);
+
+    T toDto(E entity);
+
+
+}
